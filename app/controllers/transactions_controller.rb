d65f05619979228873beed6f744be6c1418aece3
Raphael Marsan <marsanraphael@gmail.com>
2022-06-25 18:49:03 +0200

    basic implenetation for chartkick and controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,14 @@
 class TransactionsController < ApplicationController
   def index
+    @state_select = Transaction.distinct.pluck(:state)
+    sql = "SELECT DISTINCT EXTRACT(YEAR FROM order_date)::Integer from TRANSACTIONS"
+    @year_select = ActiveRecord::Base.connection.execute(sql).values.flatten.compact! #check years for that state?
     
+    @transactions = Transaction.where(state: params[:state]).where('extract(year from order_date) = ?', params[:year]) #per state per year
+
+
+    @revenue = @transactions.sum(&:sales)
+    @revenue_per_order = (@revenue/@transactions.count).round
+    @customers_count = @transactions.count("DISTINCT customer_id")
   end
 end
