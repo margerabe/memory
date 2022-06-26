dcd35c6d946d8415042a4a493cb9ce291fd39d80
Raphael Marsan <marsanraphael@gmail.com>
2022-06-26 12:16:18 +0200

    basic functional implementation

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,14 +1,31 @@
 class TransactionsController < ApplicationController
   def index
-    @state_select = Transaction.distinct.pluck(:state)
-    sql = "SELECT DISTINCT EXTRACT(YEAR FROM order_date)::Integer from TRANSACTIONS"
-    @year_select = ActiveRecord::Base.connection.execute(sql).values.flatten.compact! #check years for that state?
+    @state_select = Transaction.distinct.pluck(:state).sort
+    @year_select = ActiveRecord::Base.connection.execute(sql_years).values.flatten.compact!.sort
     
-    @transactions = Transaction.where(state: params[:state]).where('extract(year from order_date) = ?', params[:year]) #per state per year
+    @transactions = Transaction.where(state: params[:state]).where('extract(year from order_date) = ?', params[:year])
+    
+    @revenue = revenue
+    @revenue_per_order = revenue_per_order
+    @customers = customers
+  end
+  
+  private
+  
+  def sql_years
+    "SELECT DISTINCT EXTRACT(YEAR FROM order_date)::Integer from TRANSACTIONS"
+  end
 
+  def revenue
+    @revenue_amount = @transactions.sum(&:sales).round
+    @revenue_amount != 0 ? "EUR #{@revenue_amount}" : "-"
+  end
 
-    @revenue = @transactions.sum(&:sales)
-    @revenue_per_order = (@revenue/@transactions.count).round
-    @customers_count = @transactions.count("DISTINCT customer_id")
+  def revenue_per_order
+    @revenue_amount != 0 ? "EUR #{(@revenue_amount / @transactions.count("DISTINCT order_id")).round}" : "-"
+  end
+
+  def customers
+    @revenue_amount != 0 ? @transactions.count("DISTINCT customer_id") : "-"
   end
 end
