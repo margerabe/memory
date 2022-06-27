9b5caa20f7f6ebea43fc0b35c33c02f074cb3915
Raphael Marsan <marsanraphael@gmail.com>
2022-06-27 13:19:58 +0200

    install rubocop and run

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,14 +3,14 @@ class TransactionsController < ApplicationController
     @state_select = Transaction.distinct.pluck(:state).sort
     @year_select = ActiveRecord::Base.connection.execute(sql_years).values.flatten.compact.sort
     @transactions = Transaction.where(state: params[:state]).where('extract(year from order_date) = ?', params[:year])
-    
+
     @revenue = revenue
     @distinct_orders = @transactions.count("DISTINCT order_id").round
     @distinct_customers = @transactions.count("DISTINCT customer_id")
   end
-  
+
   private
-  
+
   def sql_years
     "SELECT DISTINCT EXTRACT(YEAR FROM order_date)::Integer from TRANSACTIONS"
   end
--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -11,8 +11,11 @@ describe TransactionsController, type: :controller do
       }
     end
     let(:page)      { response.parsed_body }
-    let(:revenue)   { Transaction.where(state: params[:state]).where('extract(year from order_date) = ?', params[:year]).sum(&:sales).round }
-    
+    let(:revenue)   do
+      Transaction.where(state: params[:state]).where('extract(year from order_date) = ?',
+                                                     params[:year]).sum(&:sales).round
+    end
+
     before do
       create_list(:transaction, 100)
       create(:transaction, state: 'California', order_date: Faker::Date.between(from: '2015-01-01', to: '2015-12-31'))
@@ -26,7 +29,7 @@ describe TransactionsController, type: :controller do
       expect(page).to include("Year")
     end
 
-    it 'renders correct revenue' do
+    it 'renders the correct revenue' do
       formatted_revenue = revenue.to_s.reverse.scan(/.{1,3}/).join(',').reverse
       expect(page).to include(formatted_revenue)
     end
